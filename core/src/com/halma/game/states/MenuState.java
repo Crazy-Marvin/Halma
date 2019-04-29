82b307a934ba33f362cfc0cf3bffd852ffe9887c
Jalen Winslow <jalenwinslow@yahoo.com>
2019-04-29 16:35:17 -0700

    Push button to play

--- core/src/com/halma/game/states/MenuState.java
+++ core/src/com/halma/game/states/MenuState.java
@@ -4,33 +4,37 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.halma.game.Handler;
+import com.halma.game.ui.UIButton;
 import com.halma.game.utils.Assets;
 
 public class MenuState extends State {
+
+    private UIButton playButton;
 
     public MenuState(Handler handler) {
         super(handler);
     }
 
+    @Override
+    public void init() {
+        playButton = new UIButton(handler, Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2, Assets.playButton, UIButton.b.PLAY);
+    }
 
     @Override
     public void update(float dt) {
-        if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
-            currentState = handler.getGameState();
-            currentState.init();
-        } else if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+        playButton.update(dt);
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
             Gdx.app.exit();
         }
     }
 
     @Override
     public void render(SpriteBatch batch) {
-        
-        batch.draw(Assets.playButton, Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+        playButton.render(batch);
     }
 
     @Override
     public void dispose() {
-
+        playButton.dispose();
     }
 }
---
+++ core/src/com/halma/game/ui/UIButton.java
@@ -0,0 +1,63 @@
+package com.halma.game.ui;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.halma.game.Handler;
+import com.halma.game.gameobjects.GameObject;
+import com.halma.game.states.State;
+import com.halma.game.utils.Controls;
+
+public class UIButton extends GameObject {
+
+    public enum b {
+        PLAY
+    }
+
+    public Rectangle bounds;
+    private b button;
+    private Texture t;
+
+    public UIButton(Handler handler, int x, int y, Texture t, b button) {
+        super(handler, x , y);
+        bounds = new Rectangle(0, 0, 0, 0);
+        bounds.x = x;
+        bounds.y = y;
+        this.t = t;
+        bounds.width = t.getWidth();
+        bounds.height = t.getHeight();
+        this.button = button;
+    }
+
+    @Override
+    public void update(float dt) {
+        click();
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+        batch.draw(t, bounds.x, bounds.y, bounds.width, bounds.height);
+    }
+
+    @Override
+    public void dispose() {
+        t.dispose();
+    }
+
+    public void click() {
+        if (Gdx.input.justTouched() && bounds.contains(Controls.x, Controls.y)) {
+            clickEvent(button);
+        }
+    }
+
+    private void clickEvent(b button) {
+        switch(button) {
+            case PLAY:
+                State.setCurrentState(handler.getGameState());
+                State.getCurrentState().init();
+                break;
+            default:
+        }
+    }
+}
