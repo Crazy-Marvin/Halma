8ac5df2224eb26f89d50073daad879f60ebc3d48
Frosty-J <60154347+Frosty-J@users.noreply.github.com>
2022-08-15 19:08:12 +0100

    Remove unused import

--- core/src/app/halma/redesign/About.java
+++ core/src/app/halma/redesign/About.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
-import com.badlogic.gdx.utils.Align;
 
 import app.halma.BaseScreen;
 import app.halma.Halma;
